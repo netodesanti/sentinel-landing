c9c5cfdaf9e7fb20405c9efe4063cf271f8e1cf5
Ernesto Desanti <netodesanti@hotmail.com>
2025-06-17 12:00:52 -0600

    Use local InterVariable font as default sans-serif + spacing improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['InterVariable', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+};
